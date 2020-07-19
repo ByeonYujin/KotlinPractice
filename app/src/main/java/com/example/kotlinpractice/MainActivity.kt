926ad46c2c0a719b65455e1b093e06ff1d255cf9
ByeonYujin <63533661+ByeonYujin@users.noreply.github.com>
2020-07-19 11:29:04 +0900

    Update MainActivity.kt

--- app/src/main/java/com/example/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpractice/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.kotlinpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                TODO("Not yet implemented")
+            }
+        })
+
+        //1. Kotlin interface가 아닌 java interface
+        //2. 그 interface는 딱 하나의 메소드만 가져야 한다.
+
+        button1.setOnClickListener{
+            //to do..
+        }
     }
 }
